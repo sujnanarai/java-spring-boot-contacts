d4da8e050d47521928e04460e197ca4c6b5a1b4e
Sujnana Rai <sujnanarai@gmail.com>
2018-11-28 16:54:38 +0530

    Added SpringBootServletInitializer to deploy as war

--- spring-boot-contacts/src/main/java/com/sujnan/contacts/SpringBootContactsApplication.java
+++ spring-boot-contacts/src/main/java/com/sujnan/contacts/SpringBootContactsApplication.java
@@ -2,10 +2,17 @@ package com.sujnan.contacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootContactsApplication {
+public class SpringBootContactsApplication extends SpringBootServletInitializer {
 
+	@Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringBootContactsApplication.class);
+  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootContactsApplication.class, args);
 	}
